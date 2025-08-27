e44cf7e47bdf7058b7179e39e2c2cffd4127f74c
sabique <sabiqueislam7@gmail.com>
2025-08-27 21:22:27 +0530

    feat: add footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <p>&copy; 2023 Kowl. All rights reserved.</p>
-    </div>
-  )
+    <footer className="bg-black text-gray-400 py-6">
+      <div className="max-w-6xl mx-auto px-4 flex justify-center sm:flex-row items-center gap-4">
+        <p className="text-sm">&copy; {new Date().getFullYear()} Rae. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
