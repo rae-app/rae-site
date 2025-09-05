bb71bfdd9c1d04aefb1a712d5aec576f93485b71
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-09-05 14:41:54 +0530

    Change text on about

--- src/app/info/about/page.tsx
+++ src/app/info/about/page.tsx
@@ -1,8 +1,6 @@
-
 function page() {
   return (
     <div className="min-h-dvh w-full  px-8 pb-12 max-w-[1400px]  text-black leading-[2vw]  font-bold  text-[2vw]  flex flex-col  z-40">
-      
       <div className="flex flex-col w-full shrink-0 h-[30vh] items-center justify-center">
         <div className=" w-full text-center   h-fit z-40 ">
           {`"I would rather be dead, than build something Mid"`.toUpperCase()}
@@ -10,22 +8,34 @@ function page() {
         <div className="z-40 text-right mt-4"> - TEAM, RAE</div>
       </div>
       <div className="text-black z-40 grid grid-cols-2 grid-rows-2 gap-8">
-        <div className="bg-black rounded-xl text-white p-8 text-3xl font-medium">
-          Built by engineers challenging mediocrity. In sheer pain of watching
-        everyone scramble the web to search for tools that fit their specific
-        need, we put them all in one basket and are delivering them to your
-        doorstep. 
+        <div className="bg-zinc-950 rounded-xl relative text-white p-8 text-3xl font-medium flex">
+          <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 size-[calc(100%-24px)] z-10 bg-zinc-900 blur-md"></div>
+          <div className="z-40">
+            Built by engineers challenging mediocrity. In sheer pain of watching
+          everyone scramble the web to search for tools that fit their specific
+          need, we put them all in one basket and are delivering them to your
+          doorstep.
+          </div>
         </div>
-        <div className="size-full bg-black rounded-xl overflow-hidden relative  outline-black outline-4" >
-          <img src="/assets/images/image1.jpeg" className="absolute size-full object-cover " alt="" />
+        <div className="size-full bg-black rounded-xl overflow-hidden relative  outline-black outline-4">
+          <img
+            src="/assets/images/image1.jpeg"
+            className="absolute size-full object-cover "
+            alt=""
+          />
         </div>
-        <div className="size-full bg-black rounded-xl overflow-hidden relative  outline-black outline-4" >
-          <img src="/assets/images/build.jpg" className="absolute size-full object-cover " alt="" />
+        <div className="size-full bg-black rounded-xl overflow-hidden relative  outline-black outline-4">
+          <img
+            src="/assets/images/build.jpg"
+            className="absolute size-full object-cover "
+            alt=""
+          />
         </div>
-        <div className="bg-black rounded-xl text-white p-8 text-3xl font-medium">
-          The goal of Rae. is to not just awe you but also your friends.
-        Tighten your belts , and hold your horses , because we are bringing you
-        something truly epic. Something you would tell your grandchildren about.
+        <div className="bg-zinc-950 rounded-xl relative text-white p-8 text-3xl font-medium flex">
+          <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 size-[calc(100%-24px)] z-10 bg-zinc-900 blur-md"></div>
+          <div className="z-40">
+            The goal of Rae. is to not just awe you but also your friends. Fueled by a community, whose goal is to leave a mark on mankind. Join us as its only you that can truly help Rae rise.
+          </div>
         </div>
       </div>
     </div>
