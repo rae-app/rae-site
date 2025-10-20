9a977640abe5632b52779f2b283505e0d60d7b9c
ronish <ronish.rohan@gmail.com>
2025-10-20 15:36:20 +0530

    Make circles move on mouse

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { DiscordLogoIcon, EnvelopeIcon } from "@phosphor-icons/react/dist/ssr";
 import { useScroll, motion, useTransform, useSpring } from "motion/react";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PrimaryButton from "@/components/ui/PrimaryButton";
 import { useSmoothScroll } from "@/hooks/useScroll";
 
 export default function Landing() {
+  const [pos, setPos] = useState([0, 0]);
+  const [screen, setScreen] = useState([0, 0]);
+
   const pageRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: pageRef,
@@ -19,7 +22,26 @@ export default function Landing() {
     useTransform(scrollYProgress, [0, 1], ["0%", "-120%"]),
     { stiffness: 100, damping: 30 },
   );
-  const scroll = useSmoothScroll()
+
+  useEffect(() => {
+    function handleMove(e: MouseEvent) {
+      setPos([e.clientX - screen[0] / 2, e.clientY - screen[1] / 2]);
+    }
+    if (document) {
+      setScreen([window.innerWidth, window.innerHeight]);
+      document.addEventListener("mousemove", handleMove);
+    }
+
+    return () => {
+      document.removeEventListener("mousemove", handleMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(pos);
+  }, [pos]);
+
+  const scroll = useSmoothScroll();
   return (
     <div
       id="home"
@@ -32,13 +54,34 @@ export default function Landing() {
         initial={{ opacity: 0 }}
         className="absolute  flex sm:hidden"
       >
-        <div className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border  rounded-full " />
-        <div className="size-[800px] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full " />
-        <div className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full " />
-        <div className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border/70 rounded-full " />
-        <div className="size-[550px] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full " />
-        <div className="size-[500px] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full " />
-        <div className="size-[400px] absolute border bottom-0 translate-y-1/2 border-border/40 rounded-full " />
+        <motion.div
+          animate={{ x: pos[0] / 20, y: pos[1] / 20 }}
+          className="size-[400px] absolute border bottom-0 translate-y-1/2 border-border/40 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 30, y: pos[1] / 30 }}
+          className="size-[500px] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 45, y: pos[1] / 45 }}
+          className="size-[550px] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 50, y: pos[1] / 50 }}
+          className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border/70 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 70, y: pos[1] / 70 }}
+          className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 90, y: pos[1] / 90 }}
+          className="size-[600px] absolute border bottom-0 translate-y-1/2 border-border rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 100, y: pos[1] / 100 }}
+          className="size-[800px] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full "
+        />
       </motion.div>
       <motion.div
         style={{ left: tX }}
@@ -46,13 +89,32 @@ export default function Landing() {
         initial={{ opacity: 0 }}
         className="absolute  hidden sm:flex"
       >
-        <div className="size-[30vw] absolute border bottom-0 translate-y-1/2 border-border  rounded-full " />
-        <div className="size-[20vw] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full " />
-        <div className="size-[40vw] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full " />
-        <div className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/70 rounded-full " />
-        <div className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full " />
-        <div className="size-[50vw] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full " />
-        <div className="size-[60vw] absolute border bottom-0 translate-y-1/2 border-border/40 rounded-full " />
+        <motion.div
+          animate={{ x: pos[0] / 10, y: pos[1] / 10 }}
+          className="size-[20vw] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 20, y: pos[1] / 20 }}
+          className="size-[30vw] absolute border bottom-0 translate-y-1/2 border-border  rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 30, y: pos[1] / 30 }}
+          className="size-[40vw] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full "
+        />
+
+        <motion.div
+          animate={{ x: pos[0] / 45, y: pos[1] / 45 }}
+          className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 50, y: pos[1] / 50 }}
+          className="size-[50vw] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full "
+        />
+        <motion.div
+          transition={{ ease: "easeInOut", duration: 0.05 }}
+          animate={{ x: pos[0] / 100, y: pos[1] / 100 }}
+          className="size-[60vw] absolute border bottom-0 translate-y-1/2 border-border rounded-full "
+        />
       </motion.div>
       <motion.div
         animate={{ opacity: 0.8 }}
@@ -60,13 +122,34 @@ export default function Landing() {
         style={{ right: tX2 }}
         className="absolute right-[-30%] bottom-0  rotate-180 sm:flex hidden"
       >
-        <div className="size-[30vw] absolute border bottom-0 translate-y-1/2 border-border  rounded-full " />
-        <div className="size-[20vw] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full " />
-        <div className="size-[40vw] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full " />
-        <div className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/70 rounded-full " />
-        <div className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full " />
-        <div className="size-[50vw] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full " />
-        <div className="size-[60vw] absolute border bottom-0 translate-y-1/2 border-border/40 rounded-full " />
+        <motion.div
+          animate={{ x: pos[0] / 10, y: pos[1] / 10 }}
+          className="size-[20vw] absolute border bottom-0 translate-y-1/2 border-border/90 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 20, y: pos[1] / 20 }}
+          className="size-[30vw] absolute border bottom-0 translate-y-1/2 border-border rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 30, y: pos[1] / 30 }}
+          className="size-[40vw] absolute border bottom-0 translate-y-1/2 border-border/80 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 45, y: pos[1] / 45 }}
+          className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/70 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 50, y: pos[1] / 50 }}
+          className="size-[45vw] absolute border bottom-0 translate-y-1/2 border-border/65 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 70, y: pos[1] / 70 }}
+          className="size-[50vw] absolute border bottom-0 translate-y-1/2 border-border/60 rounded-full "
+        />
+        <motion.div
+          animate={{ x: pos[0] / 100, y: pos[1] / 100 }}
+          className="size-[60vw] absolute border bottom-0 translate-y-1/2 border-border/40 rounded-full "
+        />
       </motion.div>
       <div className=" flex flex-col z-20 p-8 sm:p-8 items-center sm:justify-center justify-start">
         <div className="text-5xl  font-roboto-serif text-accent tracking-tighter">
@@ -77,7 +160,10 @@ export default function Landing() {
           Made by developers, for developers.
         </div>
         <div className="flex sm:flex-row flex-col w-full sm:w-fit gap-2 mt-8">
-          <PrimaryButton className="h-[36px]" onClick={() => scroll.scrollToSection("waitlist")}>
+          <PrimaryButton
+            className="h-[36px]"
+            onClick={() => scroll.scrollToSection("waitlist")}
+          >
             <EnvelopeIcon weight="fill" size={20} />
             Join the waitlist
           </PrimaryButton>
